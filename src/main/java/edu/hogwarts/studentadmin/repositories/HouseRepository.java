58c6dba7e125527d1decbb17dbe975512a176481
osman-butt <butt.osman@gmail.com>
2024-02-16 22:37:27 +0100

    Added House repository

---
+++ src/main/java/edu/hogwarts/studentadmin/repositories/HouseRepository.java
@@ -0,0 +1,8 @@
+package edu.hogwarts.studentadmin.repositories;
+
+import edu.hogwarts.studentadmin.models.Course;
+import edu.hogwarts.studentadmin.models.House;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface HouseRepository extends JpaRepository<House,Integer> {
+}
