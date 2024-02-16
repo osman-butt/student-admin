4bebec69bea2937f2cc1e2a5f9c99a0a3ca51f65
osman-butt <butt.osman@gmail.com>
2024-02-16 22:00:04 +0100

    added House model

---
+++ src/main/java/edu/hogwarts/studentadmin/models/House.java
@@ -0,0 +1,35 @@
+package edu.hogwarts.studentadmin.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class House {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private String founder;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFounder() {
+        return founder;
+    }
+
+    public void setFounder(String founder) {
+        this.founder = founder;
+    }
+}
