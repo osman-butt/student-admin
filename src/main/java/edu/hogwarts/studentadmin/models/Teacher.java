aed4c4a076f129576a25decf4dd6d69da85279d5
osman-butt <butt.osman@gmail.com>
2024-02-16 22:05:58 +0100

    added Teacher model

---
+++ src/main/java/edu/hogwarts/studentadmin/models/Teacher.java
@@ -0,0 +1,99 @@
+package edu.hogwarts.studentadmin.models;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+public class Teacher {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String firstName;
+    private String middleName;
+    private String lastName;
+    private LocalDate dateOfBirth;
+    @ManyToOne
+    @JoinColumn(name = "house_id", referencedColumnName = "id")
+    private House house;
+    private boolean headOfHouse;
+    private EmpType employment;
+    private LocalDate employmentStart;
+    private LocalDate employmentEnd;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public House getHouse() {
+        return house;
+    }
+
+    public void setHouse(House house) {
+        this.house = house;
+    }
+
+    public boolean isHeadOfHouse() {
+        return headOfHouse;
+    }
+
+    public void setHeadOfHouse(boolean headOfHouse) {
+        this.headOfHouse = headOfHouse;
+    }
+
+    public EmpType getEmployment() {
+        return employment;
+    }
+
+    public void setEmployment(EmpType employment) {
+        this.employment = employment;
+    }
+
+    public LocalDate getEmploymentStart() {
+        return employmentStart;
+    }
+
+    public void setEmploymentStart(LocalDate employmentStart) {
+        this.employmentStart = employmentStart;
+    }
+
+    public LocalDate getEmploymentEnd() {
+        return employmentEnd;
+    }
+
+    public void setEmploymentEnd(LocalDate employmentEnd) {
+        this.employmentEnd = employmentEnd;
+    }
+}
