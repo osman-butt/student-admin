7f7c28a11647a0892dc25a252637981b0c639b37
osman-butt <butt.osman@gmail.com>
2024-02-17 00:25:46 +0100

    Added PUT/courses/{id}
    PUT
    /courses/{id}
    /courses/{id}/teacher
    /courses/{id}/students/{studentId}

--- src/main/java/edu/hogwarts/studentadmin/controllers/CourseController.java
+++ src/main/java/edu/hogwarts/studentadmin/controllers/CourseController.java
@@ -1,15 +1,17 @@
 package edu.hogwarts.studentadmin.controllers;
 
 import edu.hogwarts.studentadmin.models.Course;
+import edu.hogwarts.studentadmin.models.House;
 import edu.hogwarts.studentadmin.models.Student;
 import edu.hogwarts.studentadmin.models.Teacher;
 import edu.hogwarts.studentadmin.repositories.CourseRepository;
+import edu.hogwarts.studentadmin.repositories.StudentRepository;
+import edu.hogwarts.studentadmin.repositories.TeacherRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -17,9 +19,13 @@ import java.util.Optional;
 @RequestMapping("/courses")
 public class CourseController {
     private final CourseRepository courseRepository;
+    private final TeacherRepository teacherRepository;
+    private final StudentRepository studentRepository;
 
-    public CourseController(CourseRepository courseRepository) {
+    public CourseController(CourseRepository courseRepository,TeacherRepository teacherRepository,StudentRepository studentRepository) {
         this.courseRepository = courseRepository;
+        this.teacherRepository = teacherRepository;
+        this.studentRepository = studentRepository;
     }
 
     @GetMapping
@@ -67,5 +73,98 @@ public class CourseController {
             // Handle case where course is not found
             return ResponseEntity.notFound().build();
         }
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Course createCourse(@RequestBody Course course) {
+        return courseRepository.save(course);
+    }
+
+    @PutMapping({"{id}"})
+    public ResponseEntity<Course> updateCourse(@PathVariable int id, @RequestBody Course course) {
+        Optional<Course> original = courseRepository.findById(id);
+        if (original.isPresent()) {
+            Course origCourse = original.get();
+            // Update course students
+            List<Student> updatedStudents = new ArrayList<>();
+            for (Student student : course.getStudents()) {
+                Optional<Student> foundStudent = studentRepository.findById(student.getId());
+                foundStudent.ifPresent(updatedStudents::add);
+            }
+            origCourse.setStudents(updatedStudents);
+            // Update course teacher
+            Teacher newTeacher = course.getTeacher();
+            Optional<Teacher> teacher = teacherRepository.findById(newTeacher.getId());
+            teacher.ifPresent(origCourse::setTeacher);
+            if (teacher.isPresent()) {
+                origCourse.setTeacher(teacher.get());
+            } else {
+                return ResponseEntity.notFound().build();
+            }
+            // Update course info
+            origCourse.setSubject(course.getSubject());
+            origCourse.setCurrent(course.isCurrent());
+            origCourse.setSchoolYear(course.getSchoolYear());
+            Course updatedCourse = courseRepository.save(origCourse);
+            return ResponseEntity.ok().body(updatedCourse);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PutMapping("{id}/teacher")
+    public ResponseEntity<Course> updateCourseTeacher(@PathVariable int id, @RequestBody Teacher teacher) {
+        Optional<Course> original = courseRepository.findById(id);
+        if (original.isPresent()) {
+            Course origCourse = original.get();
+            // Update teacher
+            Optional<Teacher> foundTeacher = teacherRepository.findById(teacher.getId());
+            if(foundTeacher.isPresent()) {
+                origCourse.setTeacher(foundTeacher.get());
+                Course updatedCourse = courseRepository.save(origCourse);
+                return ResponseEntity.ok().body(updatedCourse);
+            } else {
+                return ResponseEntity.notFound().build();
+            }
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PutMapping("{id}/students/{studentId}")
+    public ResponseEntity<Course> addStudentToCourse(@PathVariable int id, @PathVariable int studentId) {
+        Optional<Course> original = courseRepository.findById(id);
+        if(original.isPresent()) {
+            Course origCourse = original.get();
+            // Check if student is already enrolled
+            if(isStudentPresent(origCourse.getStudents(),studentId)) {
+                return ResponseEntity.ok().body(origCourse);
+            } else {
+                List<Student> newStudentList = origCourse.getStudents();
+                Optional<Student> foundStudent = studentRepository.findById(studentId);
+                if(foundStudent.isPresent()) {
+                    newStudentList.add(foundStudent.get());
+                    origCourse.setStudents(newStudentList);
+                    Course updatedCourse = courseRepository.save(origCourse);
+                    return ResponseEntity.ok().body(updatedCourse);
+                } else {
+                    return ResponseEntity.notFound().build();
+                }
+            }
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+
+    // Check if student is already enrolled in course
+    private boolean isStudentPresent(List<Student> students, int id) {
+        for (Student student : students) {
+            if (student.getId() == id) {
+                return true;
+            }
+        }
+        return false;
     }
 }
