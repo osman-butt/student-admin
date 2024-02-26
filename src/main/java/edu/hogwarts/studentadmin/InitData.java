7f8b646e3842664aa57371dc135bc1207f3b102b
osman-butt <butt.osman@gmail.com>
2024-02-26 01:43:35 +0100

    Adding env profiles

--- src/main/java/edu/hogwarts/studentadmin/InitData.java
+++ src/main/java/edu/hogwarts/studentadmin/InitData.java
@@ -6,12 +6,14 @@ import edu.hogwarts.studentadmin.repositories.HouseRepository;
 import edu.hogwarts.studentadmin.repositories.StudentRepository;
 import edu.hogwarts.studentadmin.repositories.TeacherRepository;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
 import java.util.Set;
 
 @Component
+@Profile({"test", "dev"})
 public class InitData implements CommandLineRunner {
 
     private final CourseRepository courseRepository;
