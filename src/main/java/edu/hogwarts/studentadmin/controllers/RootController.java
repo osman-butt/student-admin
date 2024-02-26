124709fd04917c4bd9c07da4cb2da81d63471a09
osman-butt <butt.osman@gmail.com>
2024-02-26 01:32:11 +0100

    Added root controller

---
+++ src/main/java/edu/hogwarts/studentadmin/controllers/RootController.java
@@ -0,0 +1,17 @@
+package edu.hogwarts.studentadmin.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class RootController {
+    @GetMapping("")
+    public Map<String, String> welcome() {
+        Map<String, String> response = new HashMap<>();
+        response.put("message", "Welcome to student admin API");
+        return response;
+    }
+}
